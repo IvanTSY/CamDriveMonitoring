1ee19d0dcb44be00caa4180fa0e0a611b6c39448
Цыпышев Иван <tsypyshev@beward.ru>
2019-11-19 18:17:20 +0700

    "First messages to Slack on ChromeBrowser 77"

--- src/test/java/lib/apiSlack/SlackSendMessaages.java
+++ src/test/java/lib/apiSlack/SlackSendMessaages.java
@@ -21,7 +21,7 @@ public class SlackSendMessaages {
 
 // Request parameters and other properties.
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-        params.add(new BasicNameValuePair("token", "xxx")); //xoxb-646834705505-810587823079-YT97TKIfwnePMp1qQFlGNOX1
+        params.add(new BasicNameValuePair("token", "xxx"));
         params.add(new BasicNameValuePair("channel", "мониторинг_camdrive"));
         params.add(new BasicNameValuePair("text", "_"+text+"_"));
         params.add(new BasicNameValuePair("username", "Мониторинг"));
@@ -49,7 +49,7 @@ public class SlackSendMessaages {
 
 // Request parameters and other properties.
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-        params.add(new BasicNameValuePair("token", "xxx")); //xoxb-646834705505-810587823079-YT97TKIfwnePMp1qQFlGNOX1
+        params.add(new BasicNameValuePair("token", "xxx"));
         params.add(new BasicNameValuePair("channel", "тестовый_канал"));
         params.add(new BasicNameValuePair("text", "_"+text+"_"));
         params.add(new BasicNameValuePair("username", "Биба"));
